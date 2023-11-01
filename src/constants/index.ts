e3217239e3ac2ee224fb8e946877ec8fed0594b1
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-11-01 20:33:17 -0300

    feat: add socials constants

---
+++ src/constants/index.ts
@@ -0,0 +1,9 @@
+export const SOCIALS = {
+  title: "Social",
+  links: [
+    { icon: "FaInstagram", href: "https://www.instagram.com/guibus_dev/" },
+    { icon: "FaYoutube", href: "https://www.linkedin.com/in/gui-bus/" },
+    { icon: "FaTiktok", href: "https://github.com/gui-bus" },
+    { icon: "FaWhatsapp", href: "https://twitter.com/guibus_dev" },
+  ],
+};
