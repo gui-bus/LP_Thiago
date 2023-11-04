08a04c8becc891d9c743e92143d8e32df4d1d7a9
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-11-04 18:45:35 -0300

    style: change classes and add icon

--- src/components/sections/aboutSection.tsx
+++ src/components/sections/aboutSection.tsx
@@ -1,19 +1,20 @@
 "use client";
 import Image from "next/image";
+import { BsTools } from 'react-icons/bs'
 
 const AboutSection = () => {
   return (
     <section className="md:container mx-auto flex items-center justify-center flex-col mt-10 overflow-y-hidden text-tmDarkGray">
-      <div className="px-5 container w-full pb-24">
-        <p className="uppercase regular-18 mb-3 text-tmyellow">
-          Nós estamos aqui por você
+      <div className="px-5 container w-full pb-10 xl:pb-20 text-center md:text-justify">
+        <p className="uppercase regular-18 mb-3 text-tmyellow flex justify-center md:justify-start gap-2 items-center">
+          Nós estamos aqui por você <BsTools size={28}/>
         </p>
 
         <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
           <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">
             Manutenção Especializada
           </h2>
-          <p className="regular-16 text-gray-30 xl:max-w-[520px]">
+          <p className="regular-16 text-gray-30 xl:max-w-[520px] xl:text-justify">
             Na nossa oficina, garantimos o funcionamento perfeito das suas
             bicicletas graças à nossa equipe de especialistas em manutenção.
             Além de realizar as manutenções necessárias, oferecemos um
