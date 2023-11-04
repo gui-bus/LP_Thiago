7bfdd8250953c3f475c30011a6e458a7042b1db8
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-11-04 17:06:40 -0300

    feat: add gallery data

---
+++ src/components/Gallery/galleryData.ts
@@ -0,0 +1,23 @@
+const galleryData = [
+    {
+      bgImage: "bg-gallery01",
+    },
+    {
+      bgImage: "bg-gallery02",
+    },
+    {
+      bgImage: "bg-gallery03",
+    },
+    {
+      bgImage: "bg-gallery04",
+    },
+    {
+      bgImage: "bg-gallery05",
+    },
+    {
+      bgImage: "bg-gallery06",
+    },
+  ];
+  
+  export default galleryData;
+  
