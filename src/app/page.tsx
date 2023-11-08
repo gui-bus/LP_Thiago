ab59622f4d9ce9a4c6ffb8dcbfa3eb5e4dfb734a
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-11-07 22:56:35 -0300

    feat: add content to header sheet menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,6 +29,7 @@ export default function Home() {
       <TeamSection />
 
       <LocationSection />
+
       <Products />
     </main>
   );
--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -5,43 +5,63 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
-  Link,
   Button,
-  NavbarMenu,
-  NavbarMenuToggle,
-  Dropdown,
-  DropdownTrigger,
-  DropdownMenu,
-  DropdownItem,
 } from "@nextui-org/react";
 import Image from "next/image";
-
-import { FaWhatsapp, FaChevronDown } from "react-icons/fa";
+import Link from "next/link";
 import { useTheme } from "next-themes";
-
-import { HiSun, HiMoon } from "react-icons/hi";
+import { HiSun, HiMoon, HiMenu } from "react-icons/hi";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { useRouter } from "next/navigation";
+import { RiTeamFill, RiHistoryFill } from "react-icons/ri";
+import { BsTools } from "react-icons/bs";
+import { FaMapMarkerAlt, FaStar, FaHome, FaWhatsapp } from "react-icons/fa";
+import { IoMdImages } from "react-icons/io";
 
 export default function Header() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
+  const router = useRouter();
   const { setTheme, theme } = useTheme();
 
   const toggleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
   };
 
+  const navItems = [
+    { label: "Página Inicial", path: "/", icon: <FaHome size={20} /> },
+    { label: "Cursos", path: "/aulas", icon: <FaStar size={20} /> },
+    { label: "Serviços", path: "/#Serviços", icon: <BsTools size={20} /> },
+    { label: "Nossa Equipe", path: "/#Equipe", icon: <RiTeamFill size={20} /> },
+    {
+      label: "Unidades Oficina Escola",
+      path: "/#Unidades",
+      icon: <FaMapMarkerAlt size={20} />,
+    },
+  ];
+
+  const discoverItems = [
+    { label: "Galeria", path: "/aulas", icon: <IoMdImages size={20} /> },
+    {
+      label: "Nossa História",
+      path: "/#Serviços",
+      icon: <RiHistoryFill size={20} />,
+    },
+  ];
+
   return (
     <Navbar
-      onMenuOpenChange={setIsMenuOpen}
       isBlurred={false}
-      className="bg-[#292929] py-2 drop-shadow-xl z-[999]"
+      className="bg-[#292929] py-2 drop-shadow-xl"
       isBordered
     >
       <NavbarContent>
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="sm:hidden text-white"
-        />
         <NavbarBrand>
           <Link href="/">
             <Image
@@ -56,78 +76,9 @@ export default function Header() {
         </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link
-            href="#"
-            className="text-white hover:bg-default/50 px-4 py-2 rounded-full text-sm xl:text-base transition-all duration-300"
-          >
-            Sobre
-          </Link>
-        </NavbarItem>
-        <Dropdown>
-          <NavbarItem>
-            <DropdownTrigger>
-              <Button
-                disableRipple
-                className="px-4 py-2 text-white text-sm rounded-full xl:text-base transition-all duration-300"
-                endContent={<FaChevronDown />}
-                radius="sm"
-                variant="light"
-              >
-                Serviços
-              </Button>
-            </DropdownTrigger>
-          </NavbarItem>
-          <DropdownMenu
-            aria-label="ACME features"
-            className="w-[340px]"
-            itemClasses={{
-              base: "gap-4",
-            }}
-          >
-            <DropdownItem
-              key="manutencao_especializada"
-              description="Manutenção especializada para atender às suas necessidades."
-            >
-              <span className="font-semibold">Manutenção Especializada</span>
-            </DropdownItem>
-            <DropdownItem
-              key="aulas_ambiente_real"
-              description="Aulas em ambiente real para uma experiência prática."
-            >
-              <span className="font-semibold">Aulas em Ambiente Real</span>
-            </DropdownItem>
-            <DropdownItem
-              key="videoaulas_gravadas"
-              description="Acesso a vídeoaulas gravadas para aprendizado flexível."
-            >
-              <span className="font-semibold">Vídeoaulas Gravadas</span>
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-        <NavbarItem>
-          <Link
-            href="#"
-            className="text-white hover:bg-default/50 px-4 py-2 rounded-full text-sm xl:text-base transition-all duration-300"
-          >
-            Contato
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem>
           <div className="flex items-center justify-center gap-2">
-            <Button
-              color="success"
-              href="#"
-              variant="shadow"
-              endContent={<FaWhatsapp size={20} />}
-              className="text-white transition-all duration-250 ease-in-out hover:bg-green-400"
-            >
-              WhatsApp
-            </Button>
-
             <Button
               variant="solid"
               className="hoverButton"
@@ -137,26 +88,99 @@ export default function Header() {
               onClick={toggleTheme}
               isIconOnly
             />
+
+            <Sheet>
+              <SheetTrigger asChild className="text-white">
+                <Button
+                  isIconOnly
+                  className="transition-all duration-300 bg-tmyellow text-white hover:bg-tmyellowHover"
+                  endContent={<HiMenu size={28} />}
+                />
+              </SheetTrigger>
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle className="flex items-center justify-center">
+                    <Link href="/">
+                      <Image
+                        src="/logo.svg"
+                        alt="Thiago Mêcanica e Oficina Escola"
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        className="w-64 h-auto object-contain"
+                      />
+                    </Link>
+                  </SheetTitle>
+                  <SheetDescription className="text-gray-200 w-full max-w-xs mx-auto">
+                    <SheetClose asChild>
+                      <Link
+                        href="https://api.whatsapp.com/message/XAS6W42TZQO4N1?autoload=1&app_absent=0"
+                        target="_blank"
+                        className="w-full"
+                      >
+                        <Button
+                          className="bg-green-500 transition-all duration-300 hover:bg-green-400 w-full text-white font-bold mt-5"
+                          endContent={<FaWhatsapp size={24} />}
+                        >
+                          WhatsApp
+                        </Button>
+                      </Link>
+                    </SheetClose>
+                    <div className="border-b border-white my-5" />
+                    <nav className="my-5">
+                      <ul className="flex flex-col items-center justify-center w-full gap-2">
+                        {navItems.map((item, index) => (
+                          <li
+                            key={index}
+                            className="w-full flex justify-center"
+                          >
+                            <SheetClose asChild>
+                              <Button
+                                type="submit"
+                                className="w-full font-medium hover:bg-tmyellow text-white border-tmyellow transition-all duration-300"
+                                onClick={() => router.push(item.path)}
+                                endContent={item.icon}
+                                variant="bordered"
+                              >
+                                {item.label}
+                              </Button>
+                            </SheetClose>
+                          </li>
+                        ))}
+                      </ul>
+                    </nav>
+
+                    <h2 className="flex justify-center font-bold text-xl mt-4" />
+                    <div className="border-b border-white my-5" />
+                    <nav className="my-5">
+                      <ul className="flex flex-col items-center justify-center w-full gap-2">
+                        {discoverItems.map((item, index) => (
+                          <li
+                            key={index}
+                            className="w-full flex justify-center"
+                          >
+                            <SheetClose asChild>
+                              <Button
+                                type="submit"
+                                className="w-full font-medium hover:bg-tmyellow text-white border-tmyellow transition-all duration-300"
+                                onClick={() => router.push(item.path)}
+                                endContent={item.icon}
+                                variant="bordered"
+                              >
+                                {item.label}
+                              </Button>
+                            </SheetClose>
+                          </li>
+                        ))}
+                      </ul>
+                    </nav>
+                  </SheetDescription>
+                </SheetHeader>
+              </SheetContent>
+            </Sheet>
           </div>
         </NavbarItem>
       </NavbarContent>
-      <NavbarMenu className="pt-10 flex flex-col items-center">
-        <NavbarItem>
-          <Link href="#" className="text-lg">
-            Integrations
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link href="#" className="text-lg">
-            Integrations
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link href="#" className="text-lg">
-            Integrations
-          </Link>
-        </NavbarItem>
-      </NavbarMenu>
     </Navbar>
   );
 }
